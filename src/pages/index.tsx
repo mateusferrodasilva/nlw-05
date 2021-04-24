d080337236c8cce2bf0ea17e957a92967dc5f286
Mateus Ferro da Silva <mferrosilva14@gmail.com>
2021-04-24 18:27:41 -0300

    after data formatting, changed type

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,12 @@ type Episode = {
   id: string;
   title: string;
   members: string;
-  published_at: string;
+  thumbnail: string;
+  description: string;
+  url: string;
+  duration: number;
+  durationAsString: string;
+  publishedAt: string;
 }
 
 type HomeProps = {
