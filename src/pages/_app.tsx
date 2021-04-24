d7bf79beeba4cdbf83947d9dd44e6f322d729aeb
Mateus Ferro da Silva <mferrosilva14@gmail.com>
2021-04-24 16:45:13 -0300

    header and player component added to all pages through _app file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
 import '../styles/global.scss'
+
+import { Header } from '../components/Header'
+import { Player } from '../components/Player'
+
+import Styles from '../styles/app.module.scss'
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className={Styles.wrapper}>
+      <main>
+        <Header />
+        <Component {...pageProps} />
+      </main>
+      <Player />
+    </div>
+  );
 }
 
 export default MyApp
